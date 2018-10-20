03522a9b0c90f3c0fcfc0c4ed2e556a688faa864
Gary Sieling <gary@garysieling.com>
2018-10-20 10:56:56 -0400

    Working URLs for all facets

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,77 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node }) => {
+    console.log(node.internal.type)
+}
+
+  
+exports.createPages = ({ graphql, actions }) => {
+    return new Promise((resolve, reject) => {
+        graphql(`
+        query {    
+            allDataJson {
+            edges {
+                node {
+                values {
+                    title
+                    facets {
+                    title
+                    values {
+                        title
+                    }
+                    }
+                }
+                }
+            }
+            }
+        }
+        `
+        ).then(({data}) => {
+            const { createPage } = actions;
+
+            const create = (slug, facetData, facetValue, subFacetValue) => createPage({
+                path: slug,
+                component: path.resolve(`./src/pages/index.js`),
+                context: {
+                    // Data passed to context is available
+                    // in page queries as GraphQL variables.
+                    slug: slug,
+                    facets: [
+                        facetData,
+                        facetValue,
+                        subFacetValue
+                    ]
+                },
+            });
+
+            const facetData = data.allDataJson.edges[0].node;
+
+            console.log(JSON.stringify(facetData))
+        
+            const rootTitle = "All Topics";
+
+            facetData.values.map(
+                (facetValue, i) => (
+                    [
+                        create(`/${rootTitle}/${facetValue.title}`, facetData, facetValue, null),
+                        facetValue.facets !== null ? 
+                        facetValue.facets.map(
+                            (subFacet, i) => 
+                            subFacet.values.map(
+                                (subFacetValue, i) =>
+                                    create(
+                                        `/${rootTitle}/${facetValue.title}/${subFacet.title}/${subFacetValue.title}`,
+                                        facetData,
+                                        facetValue,
+                                        subFacetValue
+                                    )
+                            )
+                        ) : null
+                    ]
+                )
+            );
+                                    
+            resolve();
+        })
+    })
+}
--- src/gatsby-node.js
+++
@@ -1,3 +0,0 @@
-exports.onCreateNode = ({ node }) => {
-    console.log(node.internal.type)
-  }
